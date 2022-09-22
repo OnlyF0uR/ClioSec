3680c0e1fbe783bacf307475a810a5706e3c54c3
OnlyF0uR <f0uronvacation@protonmail.com>
2022-09-22 19:46:46 +0200

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<script>
+</script>
+
+<div class="hero">
+	<h1>Coming soon!</h1>
+</div>
+
+<style>
+	.hero{
+		width: 100%;
+		height: 100vh;
+		background-image: url(../img/bg.jpg);
+		background-size: cover;
+		background-position: center;
+		position: relative;
+		overflow: hidden;
+	}
+	.hero h1 {
+		color: whitesmoke;
+		margin: 50px 50px;
+	}
+</style>
